bf3e5d29bf5b570f1839803342cbf48062bb414e
David-Aguilar2 <ab0255032025@unab.edu.sv>
2025-04-09 22:12:23 -0600

    Funcionamiento de la suma de dos números

--- app.py
+++ app.py
@@ -1,30 +1,55 @@
-from flask import Flask , request
+from flask import Flask #Importamos la libreria Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #Crear la aplicación Flask
 
-@app.route('/', methods=['GET', 'POST'])
-def create_article():
+@app.route('/') #Definir una ruta
+def  home():
         
+        #Creación de la estructura html de la calculadora
+        #Su funcionamiento será hecho con javascript
     return """
-    <center><form method ='POST' action = "/">
+    <center>
+
+        <h1>Calculadora</h1>
         <label for ='n1'> Número 1: </label>
         <input type = 'number' id = 'n1' name = 'n1'>
         <br><br>
         <label for ='n2'> Núumero 2: </label>
         <input type = 'number' id = 'n2' name = 'n2'>
         <br><br>
-        <label for ='op'> Elige la operación: </label>
-        <select>
+        <label> Elige la operación: </label>
+        <select id="op">
         <option value="+">Sumar</option>
         <option value="-">Restar</option>
         <option value="*">Multiplicar</option>
         <option value="/">Dividir</option>
         </select>
         <br><br>
-        <input type = 'submit' value = 'Crear articulo'>
-    </form></center>
-"""    
+        <button onclick="operar()">Realizar operación</button></center>
+
+   
+        <script>
+
+        function operar(){
+        
+            const numero1 = document.getElementById('n1').value;
+            const numero2 = document.getElementById('n2').value;
+
+            const select = document.getElementById("op");
+            const operacion = select.value;
+
+            if (operacion=="+"){
+            
+                const suma=Number(numero1)+Number(numero2);
+                alert(suma)
+
+            }
+            
+        }
+
+        </script>
+    """    
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #Iniciar el servidor
 
