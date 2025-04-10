3e0e2512f69211f6d3aa5274438d3ef1bfffd1d7
David-Aguilar2 <ab0255032025@unab.edu.sv>
2025-04-10 11:33:34 -0600

    Validaciones

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def  home():
         <label for ='n1'> Número 1: </label>
         <input type = 'number' id = 'n1' name = 'n1'>
         <br><br>
-        <label for ='n2'> Núumero 2: </label>
+        <label for ='n2'> Número 2: </label>
         <input type = 'number' id = 'n2' name = 'n2'>
         <br><br>
         <label> Elige la operación: </label>
@@ -25,7 +25,9 @@ def  home():
         <option value="/">Dividir</option>
         </select>
         <br><br>
-        <button onclick="operar()">Realizar operación</button></center>
+        <button onclick="operar()">Realizar operación</button>
+        <br><br><br><br>
+        <h3 id="resultado"></h3></center>
 
    
         <script>
@@ -35,33 +37,51 @@ def  home():
             const numero1 = document.getElementById('n1').value;
             const numero2 = document.getElementById('n2').value;
 
+            document.getElementById("resultado").innerHTML = "";
+
             const select = document.getElementById("op");
             const operacion = select.value;
 
-            if (operacion=="+"){
-            
-                const suma=Number(numero1)+Number(numero2);
-                alert(suma)
+            if (numero1==="" || numero2===""){
+                
+                    alert("Error, no debes dejar campos vacios");
+                
+                }else{
+                
+                    if (operacion=="+"){
+                
+                        const suma=Number(numero1)+Number(numero2);
+                        document.getElementById("resultado").innerHTML = "El resultado es: "+suma;
 
-            }
-            if (operacion=="-"){
-            
-                const resta=Number(numero1)-Number(numero2);
-                alert(resta)
+                    }
+                    if (operacion=="-"){
+                
+                        const resta=Number(numero1)-Number(numero2);
+                        document.getElementById("resultado").innerHTML = "El resultado es: "+resta;
 
-            }
-            if (operacion=="*"){
+                    }
+                    if (operacion=="*"){
             
-                const multiplicacion=Number(numero1)*Number(numero2);
-                alert(multiplicacion)
+                        const multiplicacion=Number(numero1)*Number(numero2);
+                        document.getElementById("resultado").innerHTML = "El resultado es: "+multiplicacion;
 
-            }
-            if (operacion=="/"){
+                    }
+                    if (operacion=="/"){
+                
+                        if(numero2==0){
+                        
+                            alert("Error, no se puede dividir entre 0");
+
+                        }else{
+                            const division=Number(numero1)/Number(numero2);
+                            document.getElementById("resultado").innerHTML = "El resultado es: "+division;
+
+                        }
+
+                    }
+
+                }
             
-                const division=Number(numero1)/Number(numero2);
-                alert(division)
-
-            }
         }
 
         </script>
